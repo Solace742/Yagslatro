91c9584c380b0c3d27c5fe36674aecbdfddaf2ae
Solace742 <solace742@gmail.com>
2026-04-27 23:24:18 +1000

    Update exoplanet.lua

--- src/jokers/exoplanet.lua
+++ src/jokers/exoplanet.lua
@@ -6,7 +6,7 @@ SMODS.Joker {
         y = 1
     },
     config = {
-        extra = {mult = 0, chips = 0, mult_mod = 1, chip_mod = 10
+        extra = {mult = 0, chips = 0, mult_mod = 2, chip_mod = 10
         }
     },
     rarity = 2,
